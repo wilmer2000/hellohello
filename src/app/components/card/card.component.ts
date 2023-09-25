b8cfff728b98bc336614749cbe4fe0a9c7b5d10d
Wilmer Blanco <wilmervzla2000@gmail.com>
2023-09-25 00:15:56 -0300

    Animation added

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+  styleUrls: ['./card.component.scss'],
+  animations: [
+    trigger('cardAnim', [
+      state('in', style({ opacity: 1, transform: 'translateY(0)' })),
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateY(10px)' }),
+        animate('400ms ease-in', style({ opacity: 1, transform: 'translateY(0)' }))
+      ]),
+      transition(':leave', [
+        animate('400ms ease-out', style({ opacity: 0, transform: 'translateY(10px)' }))
+      ])
+    ])
+  ]
 })
 export class CardComponent {
 
